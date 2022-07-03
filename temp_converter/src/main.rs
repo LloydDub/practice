0ca79ad5742a52a8f065cc6fe33f660426429313
LloydDub <lloydrww@gmail.com>
2022-07-03 12:34:41 -0600

    Added temp type selection, can pick either f or c

--- temp_converter/src/main.rs
+++ temp_converter/src/main.rs
@@ -1,7 +1,14 @@
 use std::io;
 
 fn main() {
-    println!("Enter a tempreture as a number to see it in celsius and farenheit");
+    println!("Please enter c for celsius or f for farenheit");
+
+    let mut c_or_f = String::new();
+    io::stdin()
+        .read_line(&mut c_or_f)
+        .expect("Failed to read line");
+
+    println!("Enter a tempreture as a number to convert it to selected tempreture format");
 
     let mut input = String::new();
     io::stdin()
@@ -16,8 +23,11 @@ fn main() {
     let c_temp = (input - 32.0) * 5.0 / 90.0;
     let f_temp = (input * 1.8) + 32.0;
 
-    println!("The tempreture in celsius is: {}", c_temp);
-    println!("The tempreture in ferenheit is: {}", f_temp);
+    if c_or_f == "c\n" {
+        println!("The tempreture in celsius is: {}", c_temp);
+    } else if c_or_f == "f\n" {
+        println!("The tempreture in ferenheit is: {}", f_temp);
+    }
 }
 
 //this application is a learning project to convert tempretures between celsius and farenheit
